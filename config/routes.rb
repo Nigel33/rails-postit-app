7357da07593bc48bbe3c610ebced7a1177464438
Nigel <nigelhing1@gmail.com>
2019-04-26 22:33:26 +0800

    fix

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,11 @@ PostitTemplate::Application.routes.draw do
   get '/logout', to: 'sessions#destroy'
 
   resources :posts, except:[:destroy] do
-  	resources :comments, only:[:create]
+  	resources :comments, only:[:create] do
+      member do 
+        post :vote
+      end 
+    end 
     member do 
       post :vote
     end 
